9ed52642fad07a5a0d4735eb045323fb802f7262
Brinda-T <brindathanikanti1998@gmail.com>
2021-03-22 12:31:36 +0530

    modified code

--- test_count_warnings.py
+++ test_count_warnings.py
@@ -7,24 +7,13 @@ from count_warnings import is_build_promoted
 from count_warnings import main
 
 TEST_MAIN_DATA = [
-<<<<<<< HEAD
     {"data": ["count_warnings.py", "2log.txt", "1log.txt"],
      "exp_result": True},
     {"data": ["count_warnings.py", "1log.txt", "2log.txt"],
      "exp_result": False},
-=======
-        {"data": ["count_warnings.py", "2log.txt", "1log.txt"],
-         "exp_result": True},
-        {"data": ["count_warnings.py", "1log.txt", "2log.txt"],
-         "exp_result": False}
-        ]
->>>>>>> 29663a02e38303118103e2ba631994f4574580a5
+]
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 29663a02e38303118103e2ba631994f4574580a5
 @pytest.mark.parametrize("ipd", TEST_MAIN_DATA)
 def test_main(ipd):
     ''' main function to print program status '''
@@ -33,15 +22,6 @@ def test_main(ipd):
     assert retval == ipd["exp_result"]
 
 
-<<<<<<< HEAD
-TEST_IS_MAIN_DATA_EXCP = [
-    {"data": ["count_warnings.py", "1log.txt"],
-     "exp_result":SystemExit}
-]
-
-
-@pytest.mark.parametrize("ipd", TEST_IS_MAIN_DATA_EXCP)
-=======
 TEST_MAIN_DATA_EXCP = [
         {"data": ["count_warnings.py", "1log.txt"],
          "exp_result":SystemExit}
@@ -49,37 +29,22 @@ TEST_MAIN_DATA_EXCP = [
 
 
 @pytest.mark.parametrize("ipd", TEST_MAIN_DATA_EXCP)
->>>>>>> 29663a02e38303118103e2ba631994f4574580a5
 def test_main_data_excpection(ipd):
     """ main data exceptions function block """
     with pytest.raises(ipd["exp_result"]):
         main(ipd["data"])
 
 
-
-<<<<<<< HEAD
-TEST_IS_VALID_ARGUMENTS_DATA = [
-    {"data": ["vinay", "1log.txt", "2log.txt"], "exp_result": True},
-    {"data": ["vinay", "1log.txt"], "exp_result": False},
-    {"data": ["vinay"], "exp_result": False},
-    {"data": ["vinay", "1log.txt", "2log.txt", "hggkj"], "exp_result": True},
-    {"data": [" "], "exp_result": False},
-]
-
-
-@pytest.mark.parametrize("ipd", TEST_IS_VALID_ARGUMENTS_DATA)
-=======
 TEST_IS_VALID_ARUGUMENTS_DATA = [
-        {"data": ("vinay", "1log.txt", "2log.txt"), "exp_result": True},
-        {"data": ("vinay", "1log.txt"), "exp_result": False},
-        {"data": ("vinay"), "exp_result": False},
-        {"data": ("vinay", "1log.txt", "2log.txt", "hggkj"), "exp_result": False},
-        {"data": (" "), "exp_result": False}
+        {"data": ["vinay", "1log.txt", "2log.txt"], "exp_result": True},
+        {"data": ["vinay", "1log.txt"], "exp_result": False},
+        {"data": ["vinay"], "exp_result": False},
+        {"data": ["vinay", "1log.txt", "2log.txt", "hggkj"], "exp_result": True},
+        {"data": [" "], "exp_result": False}
         ]
 
 
 @pytest.mark.parametrize("ipd", TEST_IS_VALID_ARUGUMENTS_DATA)
->>>>>>> 29663a02e38303118103e2ba631994f4574580a5
 def test_is_valid_aruguments(ipd):
     """ function to get given arguments are valid or not valid """
     retval = is_valid_aruguments(ipd["data"])
@@ -87,17 +52,6 @@ def test_is_valid_aruguments(ipd):
     assert retval == ipd["exp_result"]
 
 
-<<<<<<< HEAD
-TEST_GET_WARN_COUNT_DATA_BY_FILE = [
-    {"data": "1log.txt", "exp_result": 4},
-    {"data": "2log.txt", "exp_result": 5},
-    {"data": "3log.txt", "exp_result": 0},
-    {"data": " ", "exp_result": 0}
-]
-
-
-@pytest.mark.parametrize("ipd", TEST_GET_WARN_COUNT_DATA_BY_FILE)
-=======
 TEST_GET_WARN_COUNT_BY_FILE_DATA = [
         {"data": "1log.txt", "exp_result": 4},
         {"data": "2log.txt", "exp_result": 5},
@@ -107,7 +61,6 @@ TEST_GET_WARN_COUNT_BY_FILE_DATA = [
 
 
 @pytest.mark.parametrize("ipd", TEST_GET_WARN_COUNT_BY_FILE_DATA)
->>>>>>> 29663a02e38303118103e2ba631994f4574580a5
 def test_get_warn_count_by_file(ipd):
     """ function to get number read data from the given file """
     retval = get_warn_count_by_file(ipd["data"])
@@ -115,16 +68,6 @@ def test_get_warn_count_by_file(ipd):
     assert retval == ipd["exp_result"]
 
 
-<<<<<<< HEAD
-TEST_COUNT_WARNINGS_DATA = [
-    {"data": ["warning", "is warning", "warning  warning"], "exp_result": 4},
-    {"data": ["warning", "is the warning", "warning"], "exp_result": 3},
-    {"data": [''], "exp_result": 4},
-]
-
-
-@pytest.mark.parametrize("ipd", TEST_COUNT_WARNINGS_DATA)
-=======
 TEST_GET_WARNING_COUNT_DATA = [
         {"data": ["warning", "is warning", "warning  warning"], "exp_result": 4},
         {"data": ["warning", "is the warning", "warning"], "exp_result": 3},
@@ -133,26 +76,18 @@ TEST_GET_WARNING_COUNT_DATA = [
 
 
 @pytest.mark.parametrize("ipd", TEST_GET_WARNING_COUNT_DATA)
->>>>>>> 29663a02e38303118103e2ba631994f4574580a5
 def test_get_warning_count(ipd):
-    """ function tp get no of warnings of given data """
+    """function to get warning count"""
     retval = get_warning_count(ipd["data"])
     print(retval)
     assert retval == ipd["exp_result"]
 
 
-<<<<<<< HEAD
-TEST_IS_BUILD_PROMOTED_DATA = [
-    {"data": [['1log.txt', 4], ['2log.txt', 5]], "exp_result": -1},
-    {"data": [['1log.txt', 5], ['2log.txt', 4]], "exp_result": 0},
-]
-=======
 
 TEST_IS_BUILD_PROMOTED_DATA = [
         {"data": [['1log.txt', 4], ['2log.txt', 5]], "exp_result": -1},
         {"data": [['1log.txt', 5], ['2log.txt', 4]], "exp_result": 0}
         ]
->>>>>>> 29663a02e38303118103e2ba631994f4574580a5
 
 
 @pytest.mark.parametrize("ipd", TEST_IS_BUILD_PROMOTED_DATA)
@@ -163,27 +98,14 @@ def test_is_build_promoted(ipd):
     assert retval == ipd["exp_result"]
 
 
-<<<<<<< HEAD
-TEST_IS_BUILD_PROMO_EXCE = [
-    {"data": [['1log.txt'], ['2log.txt']], "exp_result": IndexError},
-    {"data": [[], []], "exp_result": IndexError},
-]
-=======
->>>>>>> 29663a02e38303118103e2ba631994f4574580a5
-
 TEST_IS_BUILD_PROMO_EXCE = [
         {"data": [['1log.txt'], ['2log.txt']], "exp_result": IndexError},
         {"data": [[], []], "exp_result": IndexError}
         ]
 
-<<<<<<< HEAD
-@pytest.mark.parametrize("ipd", TEST_IS_BUILD_PROMO_EXCE)
-def test_is_build_promo_exce(ipd):
-=======
 
 @pytest.mark.parametrize("ipd", TEST_IS_BUILD_PROMO_EXCE)
 def test_is_build_promoted_exception(ipd):
->>>>>>> 29663a02e38303118103e2ba631994f4574580a5
     """ function to get status of non-valid data """
     with pytest.raises(ipd["exp_result"]):
         is_build_promoted(ipd["data"])
